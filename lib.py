b42b93038e3aeb09ac3bed32501606b776ce3d54
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2024-06-15 10:29:53 +0100

    added python imports

---
+++ lib.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from tensorflow.keras.applications import EfficientNetB0
+
+def build_model():
+    base_model = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    model = Sequential([
+        base_model,
+        GlobalAveragePooling2D(),
+        Dropout(0.5),
+        Dense(1, activation='sigmoid')
+    ])
+    model.build(input_shape=(None, 224, 224, 3))
+    model.load_weights('./model_weights.weights.h5')
+    return model
+
+def load_and_preprocess_image(image_path, target_size=(224, 224)):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, target_size)
+    return image
+
+def cat_or_dog(image_src: str):
+    image = load_and_preprocess_image(image_src)
+    img_b = np.expand_dims(image, axis=0)
+    model = build_model()
+    return 'cat' if model.predict(img_b)[0] < 0.5 else 'dog'
---
+++ test.py
@@ -0,0 +1,4 @@
+from lib import cat_or_dog
+
+result = cat_or_dog('./internet_images/images.jpeg')
+print(result)
